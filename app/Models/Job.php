7133af55baedd43a007b02df839fdf53b0ec00e1
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2024-10-23 21:33:10 +1100

    added job migration

---
+++ app/Models/Job.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+
+class Job
+{
+    // use HasFactory;
+
+    public static function all()
+    {
+        $jobs = [
+            [
+                'id' => '1',
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => '2',
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => '3',
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ];
+
+        return $jobs;
+    }
+
+    public static function find($id)
+    {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if (!$job) {
+            abort(404);
+        }
+    }
+}
---
+++ resources/views/job.blade.php
@@ -0,0 +1,7 @@
+<x-layouts>
+    <x-slot:heading>Job</x-slot:heading>
+    <h2 class="font-bold text-lg">{{$job['title']}}</h2>
+    <p>
+        This job pays {{$job['salary']}} per year.
+    </p>
+</x-layouts>
