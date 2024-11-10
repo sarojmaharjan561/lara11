5c09ae8e5ca9ac3f762359ed901cd58690ecaa4c
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2024-11-10 17:55:03 +1100

    added pivot tables for tags

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Arr;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
@@ -18,5 +20,10 @@ class Job extends Model
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
     }
 }
