7b2a0ecbfd7d519c4619a980f5fdd1be04bf6d90
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2024-12-09 19:39:46 +1100

    code clean up

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/', 'home');
-
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::post('/job', [JobController::class, 'store']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
 Route::view('/contact', 'contact');
+
+Route::resource('/jobs', JobController::class);
