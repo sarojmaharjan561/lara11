ad8441a5297d322028f1b572f8a6007d23a26381
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2024-12-12 19:19:34 +1100

    added form components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,39 +1,27 @@
 <x-layouts>
     <x-slot:heading>Create a new Job</x-slot:heading>
 
-    <form method="POST" action="/job">
+    <form method="POST" action="/jobs">
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="Job Title" required>
-                            </div>
-                            @error('title')
-                            <p class="text-red-500 text-xs text-semiblod mt-1">
-                                {{$message}}
-                            </p>
-                            @enderror
+                            <x-form-input type="text" name="title" id="title" placeholder="Job Title" required></x-form-input>
+                            <x-form-error name="title" />
                         </div>
-                    </div>
+                    </x-form-field>
 
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="$50,000 per year" required>
-                            </div>
-                            @error('salary')
-                            <p class="text-red-500 text-xs text-semiblod mt-1">
-                                {{$message}}
-                            </p>
-                            @enderror
+                            <x-form-input type="text" name="salary" id="salary" placeholder="$50,000 per year" required></x-form-input>
+                            <x-form-error name="salary" />
                         </div>
-                    </div>
+                    </x-form-field>
                 </div>
             </div>
 
